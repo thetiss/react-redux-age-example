785364fc8057b3775bf8ff080fa3fefc83593aab
thesis <1711327451@qq.com>
2021-02-08 15:01:15 +0800

    feat(pages): 添加history列表，点击item,可删除该item

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import { connect } from "react-redux";
 import  styles  from "./App.module.css";
-const App = ( { currentAge, onAgeUp, onAgeDown} ) => {
+const App = ( { currentAge, ageHistory, onAgeUp, onAgeDown, onHistoryItemDeleted } ) => {
   return (
     <div className={styles.Age}>
       <div><span className={styles.Age_title}>Age: { currentAge }</span></div>
       <button onClick={ onAgeUp }>Age up(by 5)</button>
       <button onClick={ onAgeDown }>Age down(by 1)</button>
-      {/* <hr/>
-      <div>
+      <hr/>
+      <div><h1>Age history, You can delete!</h1>
         <ul>
-
+         { ageHistory && ageHistory.map(( item, index ) => {
+            return(
+              <li 
+                key={item.id} 
+                onClick={ () => onHistoryItemDeleted(item.id)} 
+                className={ styles.Age_historyItem }>
+                {item.id}_{item.value}
+              </li>    
+            ) 
+         }   
+          ) 
+         }
         </ul>
-      </div> */}
+      </div>
     </div>
   );
 };
 const mapStateToProps = ( state ) => {
   return {
     currentAge: state.age,
+    ageHistory: state.ageHistory,
   }
 };
 const mapDispatchToProps = ( dispatch ) => {
@@ -33,7 +45,13 @@ const mapDispatchToProps = ( dispatch ) => {
         payload: {
           val: 1
         }
-      })
+      }),
+    onHistoryItemDeleted: (itemId) => dispatch({
+        type: 'DELETE_AGE_FROM_HISTORY',
+        payload: {
+          val: itemId
+        }
+    })
   }  
 }
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,22 +1,32 @@
+import { act } from "react-dom/test-utils";
+const idFix = Math.random().toString(16).substr(2, 8);
+
 const initState = {
     age: 21,
+    ageHistory: [],
 }
 
 const reducer = ( state = initState, action ) => {
     const newState = { ...state };
-    switch (action.type) {
+    switch ( action.type ) {
         case 'AGE_UP':
-            console.log("in reducer"+action.type);
             newState.age += action.payload.val;
-            console.log(action.payload.val);
-            console.log("new age"+newState.age);
+            newState.ageHistory = newState.ageHistory.concat({
+                id: idFix+newState.ageHistory.length,
+                value: newState.age
+            });
             break;
         case 'AGE_DOWN':
-            console.log("in reducer"+action.type);
             newState.age -= action.payload.val;
-            console.log(action.payload.val);
-            console.log("new age"+newState.age);
+            newState.ageHistory = newState.ageHistory.concat({
+                id: idFix+newState.ageHistory.length,
+                value: newState.age
+            });
             break;
+        case 'DELETE_AGE_FROM_HISTORY':
+            const itemId = action.payload.val;
+            newState.ageHistory = newState.ageHistory.filter((item)=> item.id!==itemId);
+            break;                    
         default:
             newState.age += 0;                         
     }
